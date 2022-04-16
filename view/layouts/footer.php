8c79a021bd4ae4a1fac496d388cd02820de66826
unknown <kingsleytwum2@gmail.com>
2022-04-16 08:57:42 +0000

    refactored my footer codes

--- view/layouts/footer.php
+++ view/layouts/footer.php
@@ -1,23 +1,22 @@
-<footer>
-  <div class="container">
-  <div>
+ <footer>
+  <div class="container d-flex justify-content-between py-5">
+  <div class="d-flex me-2 logo">
    <img src="public/images/ucc.png" alt="ucc logo" class="img-fluid" id="ucc-footer-logo">
-   <div>
-     <h4>UNIVERSITY OF CAPE COAST</h4>
+   <div >
+     <h5>UNIVERSITY OF CAPE COAST</h5>
      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
-       eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
-       Nemo enim ipsam voluptatem</p>
+       eaque ipsa quae ab illo inventore veritatis</p>
    </div>
   </div>
 
-  <div>
+  <div class="d-flex flex-column">
   <a class="" href="#">HOW TO APPLY</a>
   <a class="" href="#">SUPPORT</a>
   <a class="" href="#">FAQ's</a>
   <a class="" href="#">REQUIREMENTS</a>
   </div>
 
-  <div>
+  <div class="d-flex flex-column" >
   <a class="" href="#">info@internships.ucc.edu.gh</a>
   <a class="" href="#">DIRECTORATES</a>
   <a class="" href="#">SECTIONS & DEPARTMENTS</a>
@@ -27,9 +26,3 @@
  
   </div>
 </footer>
-
-<!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="test.js"></script>
-  </body>
-</html>
