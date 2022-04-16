1dc843eda1db1777c2940061e2d4e882ce1e265d
unknown <kingsleytwum2@gmail.com>
2022-04-16 13:09:50 +0000

    working on dashboard

---
+++ view/dashboard.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/style.css">
+    
+    <title>Internship UCC</title>
+  </head>
+  <body>
+
+  <!-- header start -->
+   <header>
+       <nav role="navigation" class="container navbar navbar-dark navbar-expand-lg " id="nav">
+           
+           <div class="navbar-brand">
+               <img src="public/images/ucc.png" alt="ucc logo" class="img-fluid" id="ucc-logo">
+               <span> INTERNSHIP UCC</span>
+            </div>
+
+            
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+         
+
+
+            <div class="collapse navbar-collapse d-md-flex justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav d-sm-flex align-items-end align-items-sm-center ">
+                    <a class="nav-link" href="#">DEPARTMENTS</a>
+                    <a class="nav-link" href="#">FAQs</a>
+                    <a class="nav-link" href="#">REVIEWS</a>
+                    <a class="nav-link" href="#"><button class="btn" type="button" id="login-button">LOGIN</button></a>
+                    
+                </div>
+            </div>
+  
+          
+            
+    
+
+
+       </nav>
+   </header>
+
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="test.js"></script>
+  </body>
+</html>
